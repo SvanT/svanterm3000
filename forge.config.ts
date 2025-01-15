8cd7841a8187ddd959ca81b8bf8f994d93c1c2ea
Svante Boberg <svante.boberg@gmail.com>
2025-01-15 12:40:43 +0100

    Only make for windows

--- forge.config.ts
+++ forge.config.ts
@@ -1,7 +1,4 @@
-import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
-import { MakerZIP } from "@electron-forge/maker-zip";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import type { ForgeConfig } from "@electron-forge/shared-types";
 
@@ -12,12 +9,7 @@ const config: ForgeConfig = {
 			// Ignore files/directories not needed
 		],
 	},
-	makers: [
-		new MakerSquirrel({}),
-		new MakerZIP({}, ["darwin"]),
-		new MakerRpm({}),
-		new MakerDeb({}),
-	],
+	makers: [new MakerSquirrel({})],
 	plugins: [
 		new VitePlugin({
 			// `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
