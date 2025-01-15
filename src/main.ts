2dc084b3361fc3b16dd75ea83eb75ff4fd4656c4
Svante Boberg <svante.boberg@gmail.com>
2025-01-15 18:29:04 +0100

    Cleanup darwin specific code

--- src/main.ts
+++ src/main.ts
@@ -39,21 +39,8 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
-});
-
-app.on("activate", () => {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (BrowserWindow.getAllWindows().length === 0) {
-		createWindow();
-	}
+	app.quit();
 });
 
 ipcMain.on("start-terminal", (event) => {
